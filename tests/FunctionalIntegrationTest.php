bd91a89cdc1dfa12abf1a6112369fa1589d4f7e2
Christian Lück <christian@lueck.tv>
2017-09-08 18:17:39 +0200

    Update Socket to send secure HTTPS requests with PHP < 7.1.4

--- tests/FunctionalIntegrationTest.php
+++ tests/FunctionalIntegrationTest.php
@@ -47,6 +47,67 @@ class FunctionalIntegrationTest extends TestCase
     }
 
     /** @group internet */
+    public function testPostDataReturnsData()
+    {
+        $loop = Factory::create();
+        $client = new Client($loop);
+
+        $data = str_repeat('.', 33000);
+        $request = $client->request('POST', 'https://' . (mt_rand(0, 1) === 0 ? 'eu.' : '') . 'httpbin.org/post', array('Content-Length' => strlen($data)));
+
+        $buffer = '';
+        $request->on('response', function (Response $response) use (&$buffer) {
+            $response->on('data', function ($chunk) use (&$buffer) {
+                $buffer .= $chunk;
+            });
+        });
+
+        $request->on('error', 'printf');
+        $request->on('error', $this->expectCallableNever());
+
+        $request->end($data);
+
+        $loop->run();
+
+        $this->assertNotEquals('', $buffer);
+
+        $parsed = json_decode($buffer, true);
+        $this->assertTrue(is_array($parsed) && isset($parsed['data']));
+        $this->assertEquals(strlen($data), strlen($parsed['data']));
+        $this->assertEquals($data, $parsed['data']);
+    }
+
+    /** @group internet */
+    public function testPostJsonReturnsData()
+    {
+        $loop = Factory::create();
+        $client = new Client($loop);
+
+        $data = json_encode(array('numbers' => range(1, 50)));
+        $request = $client->request('POST', 'https://httpbin.org/post', array('Content-Length' => strlen($data), 'Content-Type' => 'application/json'));
+
+        $buffer = '';
+        $request->on('response', function (Response $response) use (&$buffer) {
+            $response->on('data', function ($chunk) use (&$buffer) {
+                $buffer .= $chunk;
+            });
+        });
+
+        $request->on('error', 'printf');
+        $request->on('error', $this->expectCallableNever());
+
+        $request->end($data);
+
+        $loop->run();
+
+        $this->assertNotEquals('', $buffer);
+
+        $parsed = json_decode($buffer, true);
+        $this->assertTrue(is_array($parsed) && isset($parsed['json']));
+        $this->assertEquals(json_decode($data, true), $parsed['json']);
+    }
+
+    /** @group internet */
     public function testCancelPendingConnectionEmitsClose()
     {
         $loop = Factory::create();
